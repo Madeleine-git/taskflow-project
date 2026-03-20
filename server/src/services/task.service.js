4e6db10c3c2c5e8e35b8425fd60116ae57aafb23
Madeleine Urrego <madeleine.urrego@alu.ceacfp.es>
2026-03-20 09:51:30 +0100

    feat: persistencia en archivo JSON

--- server/src/services/task.service.js
+++ server/src/services/task.service.js
@@ -1,29 +1,51 @@
-let libros = [];
+const fs = require('fs');
+const path = require('path');
+
+const DATA_PATH = path.join(__dirname, '../data/data.json');
+
+function leerLibros() {
+    try {
+        const contenido = fs.readFileSync(DATA_PATH, 'utf-8');
+        return JSON.parse(contenido);
+    } catch {
+        return [];
+    }
+}
+
+function guardarLibros(libros) {
+    fs.writeFileSync(DATA_PATH, JSON.stringify(libros, null, 2));
+}
 
 function obtenerTodos() {
-  return libros;
+    return leerLibros();
 }
 
 function crearLibro(data) {
-  const nuevoLibro = {
-    id: Date.now(),
-    ...data,
-  };
-  libros.push(nuevoLibro);
-  return nuevoLibro;
+    const libros = leerLibros();
+    const nuevoLibro = {
+        id: Date.now(),
+        ...data,
+    };
+    libros.push(nuevoLibro);
+    guardarLibros(libros);
+    return nuevoLibro;
 }
 
 function actualizarLibro(id, data) {
-  const index = libros.findIndex((l) => l.id === Number(id));
-  if (index === -1) throw new Error('NOT_FOUND');
-  libros[index] = { ...libros[index], ...data };
-  return libros[index];
+    const libros = leerLibros();
+    const index = libros.findIndex((l) => l.id === Number(id));
+    if (index === -1) throw new Error('NOT_FOUND');
+    libros[index] = { ...libros[index], ...data };
+    guardarLibros(libros);
+    return libros[index];
 }
 
 function eliminarLibro(id) {
-  const index = libros.findIndex((l) => l.id === Number(id));
-  if (index === -1) throw new Error('NOT_FOUND');
-  libros.splice(index, 1);
+    const libros = leerLibros();
+    const index = libros.findIndex((l) => l.id === Number(id));
+    if (index === -1) throw new Error('NOT_FOUND');
+    libros.splice(index, 1);
+    guardarLibros(libros);
 }
 
 module.exports = { obtenerTodos, crearLibro, actualizarLibro, eliminarLibro };
